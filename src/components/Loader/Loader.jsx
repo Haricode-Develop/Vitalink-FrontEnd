7af5f36668fb310029423724c2fa3770cef8c4aa
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-01-19 00:19:40 -0600

    Loader lottie
    Update, Actualizo el loader con una animación lottie.

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,27 @@
 // Loader.jsx
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import lottie from 'lottie-web';
 import { loaderStyle } from './LoaderStyle';
+import HeartLoaderData from './HeartLoader.json';
 
 const Loader = () => {
+    const animationContainer = useRef(null);
+
+    useEffect(() => {
+        const anim = lottie.loadAnimation({
+            container: animationContainer.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: HeartLoaderData,
+
+        });
+
+        return () => anim.destroy();
+    }, []);
+
     return (
-        <div style={loaderStyle.container}>
-            <div style={loaderStyle.loader}></div>
-            <style>
-                {`
-          @keyframes spin {
-            from { transform: rotate(0deg); }
-            to { transform: rotate(360deg); }
-          }
-        `}
-            </style>
-        </div>
+        <div style={loaderStyle.container} ref={animationContainer} />
     );
 };
 
--- src/components/Loader/LoaderStyle.js
+++ src/components/Loader/LoaderStyle.js
@@ -1,32 +1,15 @@
-// LoaderStyle.js
-
 export const loaderStyle = {
     container: {
         position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
+        top: '45%',
+        left: '47%',
+        transform: 'translate(-50%, -50%)',
+        width: '40%',
+        height: '40%',
         backgroundColor: 'rgba(255, 255, 255, 0.7)',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         zIndex: 1000,
-    },
-    loader: {
-        border: '5px solid #f3f3f3',
-        borderTop: '5px solid #3498db',
-        borderRadius: '50%',
-        width: '50px',
-        height: '50px',
-        animation: 'spin 2s linear infinite',
-    },
-    "@keyframes spin": {
-        from: {
-            transform: 'rotate(0deg)',
-        },
-        to: {
-            transform: 'rotate(360deg)',
-        },
     },
 };
