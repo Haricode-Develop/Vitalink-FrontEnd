7614c4a73ea1b550394cd04f4f263f7a3dbd7cd3
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-04-14 20:50:25 -0600

    Service Worker
    Feature, adjunto primera funcionalidad para aplicar logica de cache.

---
+++ src/utils/webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+module.exports = {
+    plugins: [
+        new InjectManifest({
+            swSrc: './src/sw.js',
+            swDest: 'service-worker.js',
+        }),
+    ],
+};
